a96e887d28c902725e719ef5ecb2fa3b75300fb1
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-21 18:03:43 -0600

    Prints out information about a student, practice concatenation.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World from the IntelliJ IDE!");
+
+    String studentFirstName = "Kacey";
+    String studentLastName = "Brance";
+    int studentAge = 17;
+    double studentGpa = 3.95;
+    char firstInitial = studentFirstName.charAt(0);
+    char lastInitial = studentLastName.charAt(0);
+    boolean hasPerfectAttendance = true;
+
+    System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGpa);
+    System.out.println(studentAge);
+    System.out.println(hasPerfectAttendance);
   }
 }
