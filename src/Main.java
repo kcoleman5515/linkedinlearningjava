8b7589212aa05c20d3c991d81053c1afea37a9ed
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-21 18:13:32 -0600

    Receives input to change GPA (not permanent

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
+
     String studentFirstName = "Kacey";
     String studentLastName = "Brance";
-    int studentAge = 17;
     double studentGpa = 3.95;
-    char firstInitial = studentFirstName.charAt(0);
-    char lastInitial = studentLastName.charAt(0);
-    boolean hasPerfectAttendance = true;
+//    int studentAge = 17;
+//    char firstInitial = studentFirstName.charAt(0);
+//    char lastInitial = studentLastName.charAt(0);
+//    boolean hasPerfectAttendance = true;
 
     System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGpa);
-    System.out.println(studentAge);
-    System.out.println(hasPerfectAttendance);
+    System.out.println("What do you want to update it to?");
+
+    Scanner input = new Scanner(System.in);
+    studentGpa = input.nextDouble();
+
+    System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGpa);
   }
 }
