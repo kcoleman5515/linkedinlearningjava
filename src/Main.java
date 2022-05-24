b8df885819240c04822148c0fd7c6551799c2be5
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-24 15:20:06 -0600

    If and while using song repeat button

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args)  {
-    System.out.println("Pick a number between 1 and 10");
-    Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    boolean isOnRepeat = true;
+    while(isOnRepeat) {
+      System.out.println("Playing current song");
+      System.out.println("Would you like to take this song off repeat? If so, answer yes.");
+      String userInput = input.next();
 
-    int inputtedNum = scanner.nextInt();
-
-    if (inputtedNum < 5) {
-      System.out.println("Enjoy the good luck a friend brings you");
-    } else {
-      System.out.println("Your shoe selection will alter your reality");
+      if(userInput.equals("yes")) {
+        isOnRepeat = false;
+      }
     }
+    System.out.println("Playing next song");
   }
-
 }
