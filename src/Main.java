18628ce66e6e329f1fc0aecfb7495e96e14d8549
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-25 14:37:21 -0600

    Calculate split checks

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,21 @@ public class Main {
 //    System.out.println("It's developer tea time!");
 //  }
 
-  public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+  public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
     System.out.println("What is my total owed for my meal?");
     double tip = tipRate * listedMealPrice;
     double tax = taxRate * listedMealPrice;
     double result = listedMealPrice + tip + tax;
-    System.out.println("Your total meal price is " + result);
+    return result;
 
   }
 
   public static void main(String[] args) {
 
-    calculateTotalMealPrice(22.5, .2, .08);
+    double groupTotalMealPrice = calculateTotalMealPrice(110, .2, .08);
+    System.out.println(groupTotalMealPrice);
 
+    double individualMealPrice = groupTotalMealPrice / 5;
+    System.out.println(individualMealPrice);
   }
 }
