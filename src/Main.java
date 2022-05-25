49f43e46699d9b01b9f616e089a91eedf3962f7a
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-25 08:33:32 -0600

    Calculate tip and tax example

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void announceDeveloperTeaTime() {
-    System.out.println("Waiting for developer tea time...");
-    System.out.println("Type in any word and press enter to start developer tea time");
-    Scanner input = new Scanner(System.in);
-    input.next();
-    System.out.println("It's developer tea time!");
-  }
+//  public static void announceDeveloperTeaTime() {
+//    System.out.println("Waiting for developer tea time...");
+//    System.out.println("Type in any word and press enter to start developer tea time");
+//    Scanner input = new Scanner(System.in);
+//    input.next();
+//    System.out.println("It's developer tea time!");
+//  }
 
   public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
     System.out.println("What is my total owed for my meal?");
     double tip = tipRate * listedMealPrice;
     double tax = taxRate * listedMealPrice;
     double result = listedMealPrice + tip + tax;
-    System.out.println("What is my total owed for my meal?");
+    System.out.println("Your total meal price is " + result);
 
   }
 
   public static void main(String[] args) {
-    System.out.println("Welcome to your new job");
 
-    announceDeveloperTeaTime();
+    calculateTotalMealPrice(22.5, .2, .08);
 
-    System.out.println("Write code");
-    System.out.println("Review code");
-
-    announceDeveloperTeaTime();
-
-    System.out.println("Get promoted!");
   }
 }
