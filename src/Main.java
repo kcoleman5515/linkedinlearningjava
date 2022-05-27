749c7057f689767a1bf1188b4f1f8d3e33f7c684
Kyle Coleman <kcoleman20@cnm.edu>
2022-05-27 13:59:01 -0600

    Triangle instance method created

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,14 @@
 
 public class Main {
-//  public static void announceDeveloperTeaTime() {
-//    System.out.println("Waiting for developer tea time...");
-//    System.out.println("Type in any word and press enter to start developer tea time");
-//    Scanner input = new Scanner(System.in);
-//    input.next();
-//    System.out.println("It's developer tea time!");
-//  }
 
-  public void Triangle(double base, double height,
-      double sideLenOne, double sideLenTwo, double sideLenThree) {
-    
-  }
-
-
-  public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
-    System.out.println("What is my total owed for my meal?");
-    double tip = tipRate * listedMealPrice;
-    double tax = taxRate * listedMealPrice;
-    double result = listedMealPrice + tip + tax;
-    return result;
-
-  }
-
-  public static double calculateSalary(double hourlyPay, double hoursWorkedPerWeek, int vacationDays) {
-    if(hoursWorkedPerWeek < 0) {
-      return - 1;
-    }
-    if(hourlyPay < 0) {
-      return - 1;
-    }
-
-    double weeklyPaycheck = hourlyPay * hoursWorkedPerWeek;
-    double unpaidTime = vacationDays * hourlyPay * 8;
-    return (weeklyPaycheck * 52) - unpaidTime;
-  }
 
   public static void main(String[] args) {
-    double salary = calculateSalary(18, 40, 8);
-    System.out.println("Estimated yearly salary before tax is " + salary);
+    Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+    Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
+
+    double triangleAArea = triangleA.findArea();
+    System.out.println(triangleAArea);
 
 
-//    double groupTotalMealPrice = calculateTotalMealPrice(110, .2, .08);
-//    System.out.println(groupTotalMealPrice);
-//
-//    double individualMealPrice = groupTotalMealPrice / 5;
-//    System.out.println(individualMealPrice);
   }
 }
---
+++ src/Triangle.java
@@ -0,0 +1,28 @@
+public class Triangle {
+
+  // Properties
+  double base;
+  double height;
+  double sideLenOne;
+  double sideLenTwo;
+  double sideLenThree;
+
+  // Behaviors
+  public Triangle(double base, double height,
+      double sideLenOne, double sideLenTwo, double sideLenThree) {
+    this.base = base;
+    this.height = height;
+    this.sideLenOne = sideLenOne;
+    this.sideLenTwo = sideLenTwo;
+    this.sideLenThree = sideLenThree;
+  }
+
+  public double findArea() {
+    return (this.base * this.height) / 2;
+  }
+
+  public void calculateTriangleType() {
+    // Equilateral, isosceles, or selene
+  }
+
+}
